7680a2dca655a9ebb5da2c381591263c2e1b6cd0
Lucas5990 <lucasandrade5990@gmail.com>
2021-10-16 11:58:15 -0300

    Session 2 - Duplication

---
+++ cypress/integration/duplication/sample1.spec.js
@@ -0,0 +1,29 @@
+describe("Code duplication bad practice - Sample 1", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/search**").as("getStories");
+    cy.visit("https://hackernews-seven.vercel.app");
+    cy.wait("@getStories");
+    cy.get('input[type="text"]')
+      .as("searchInput")
+      .should("be.visible")
+      .and("have.value", "redux")
+      .clear();
+  });
+  it("searches by typing and hitting enter", () => {
+    cy.get("@searchInput").type("frontend testing{enter}");
+
+    cy.wait("@getStories");
+
+    cy.get(".table-row").should("have.length", 100);
+  });
+
+  it("searches by typing and pressing the search button", () => {
+    cy.get("@searchInput").type("frontend testing");
+
+    cy.contains("button", "Search").should("be.visible").click();
+
+    cy.wait("@getStories");
+
+    cy.get(".table-row").should("have.length", 100);
+  });
+});
---
+++ cypress/integration/duplication/sample2.spec.js
@@ -0,0 +1,20 @@
+describe("Code duplication bad practice - Sample 2", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "**/search**").as("getStories");
+
+    cy.visit("https://hackernews-seven.vercel.app");
+    cy.wait("@getStories");
+  });
+
+  const terms = ["reactjs", "vuejs"];
+
+  terms.forEach((term) => {
+    it(`seaches for ${term}`, () => {
+      cy.search(term);
+
+      cy.wait("@getStories");
+
+      cy.get(".table-row").should("have.length", 100);
+    });
+  });
+});
